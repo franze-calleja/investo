0539522546f7c4d9397d09b52786e9ca03e19f6a
franze-calleja <franze.calleja@mseuf.edu.ph>
2025-12-27 15:09:13 +0800

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="px-20"
+    >
+      <Text className="text-xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
